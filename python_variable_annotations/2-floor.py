63890aa64ef510c29fc452e1c25146e71053e21a
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-06 14:17:49 +0100

    add shebang

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,2 +1,5 @@
+#!/usr/bin/env python3
+
+
 def floor(n: float) -> int:
     return int(n) if n >= 0 else int(n) - 1
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 from typing import Iterable, List, Sequence, Tuple
 
 
