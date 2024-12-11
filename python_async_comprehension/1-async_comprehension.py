3f1ac67c9f176828f4cd4a55d7e555052aa2a785
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-11 14:28:09 +0100

    The coroutine will collect 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers.

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> typing.Generator[float, None, None]:
+    '''coroutine will loop 10 times, each time asynchronously wait 1 second'''
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+
+async def async_comprehension():
+    return [num async for num in async_generator()][:10]
