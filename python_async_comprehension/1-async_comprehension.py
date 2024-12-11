32c2b86596fe43a1dbc463dab419361372ab084b
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-11 14:37:24 +0100

    import task 0

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,13 +2,7 @@
 import asyncio
 import random
 import typing
-
-
-async def async_generator() -> typing.Generator[float, None, None]:
-    '''coroutine will loop 10 times, each time asynchronously wait 1 second'''
-    for i in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> typing.Generator[float, None, None]:
