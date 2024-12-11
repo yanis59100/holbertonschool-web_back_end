fbfcba945966d25783885d0e6115ff52a16c3fa5
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-11 15:12:08 +0100

    add typing

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,9 @@
 import asyncio
 import random
 import typing
-from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension() -> typing.List[float]:
     '''then return the 10 random numbers.'''
     return [number async for number in async_generator()]
