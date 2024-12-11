a5f1f1f66a98a2116ca5cc6447d5bc50cbe77bf5
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-11 14:25:08 +0100

    add typing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 ''' async_generator '''
 import asyncio
 import random
+import typing
 
 
-async def async_generator():
+async def async_generator() -> typing.Generator[float, None, None]:
     '''coroutine will loop 10 times, each time asynchronously wait 1 second'''
     for i in range(10):
         await asyncio.sleep(1)
