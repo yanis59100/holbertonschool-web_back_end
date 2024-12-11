468b7b1caa0d615b23e37c9e9548c727e2fa765a
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-11 14:08:12 +0100

    add comment

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+''' async_generator '''
 import asyncio
 import random
 
 
 async def async_generator():
+    '''coroutine will loop 10 times, each time asynchronously wait 1 second'''
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
