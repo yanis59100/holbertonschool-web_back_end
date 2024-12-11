5a2c72df8be77042973cb100f5e154cd7f8486cf
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-11 15:07:04 +0100

    measure_runtime should measure the total runtime and return it. Notice that the total runtime is roughly 10 seconds, explain it to yourself.

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+import typing
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    before_e = time.perf_counter()
+    e = await asyncio.gather(*(async_comprehension() for i in range(4)))
+    after_e = time.perf_counter() - before_e
+    return after_e
