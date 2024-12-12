c9b475d79b17096812d861c05e564201a85fd8b3
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-12 17:53:59 +0100

    add comments

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+'''momgo collection'''
 
 
 def list_all(mongo_collection):
     '''Return an empty list if no document in the collection'''
     result = mongo_collection.school.find()
-    if result:
-        return result
-    return []
+    if result == 0:
+        return []
+    return result
