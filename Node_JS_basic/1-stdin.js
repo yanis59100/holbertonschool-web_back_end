7855f14ecfe3ab4043942db0afa3ac8b9c0fda0f
yanis59100 <meftouh.yanis@hotmail.com>
2025-01-23 15:40:34 +0100

    add new line

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,10 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.exit();
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  console.log('This important software is now closing\n');
 });
 
